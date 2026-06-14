93b548e5c1f369b3795acde24387821c519bc2bb
Matvei55 <mosy12dunaev@gmail.com>
2026-06-15 00:46:44 +0600

    сделал класс Get

---
+++ App/Get.php
@@ -0,0 +1,36 @@
+<?php
+namespace App;
+class Get
+{
+    private array $params;
+    public function __construct()
+    {
+        $this->params = $_GET;
+    }
+
+
+    public function get(string $key, $default = null): mixed
+    {
+        return $this->params[$key] ?? $default;
+    }
+
+    public function all(): array
+    {
+        return $this->params;
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($this->params[$key]);
+    }
+
+    public function getInt(string $key, int $default = 0): int
+    {
+        return (int)($this->params[$key] ?? $default);
+    }
+
+    public function getString(string $key, string $default = ''): string
+    {
+        return trim(htmlspecialchars($this->params[$key] ?? $default));
+    }
+}
